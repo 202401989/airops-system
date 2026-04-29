e98e7ee89c3bf2b8e447b94675746f74439644d6
202401989 <202401989@estudiante.udv.edu.gt>
2026-04-28 23:22:23 -0600

    Primer commit AirOps

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.static(__dirname));
+app.use(express.json());
+
+//  CONNECTION STRING 
+const connectionString = 'postgresql://postgres:AirOps2026db@db.vgwcvvsecmtttjoducxd.supabase.co:5432/postgres';
+
+console.log("Connection string:", connectionString);
+
+const pool = new Pool({
+  connectionString,
+  ssl: { rejectUnauthorized: false }
+});
+
+//  Test conexión
+app.get('/test', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ ok: true, time: result.rows[0] });
+  } catch (error) {
+    console.error('❌ ERROR CONEXIÓN:', error);
+    res.status(500).json({
+      error: error.message
+    });
+  }
+});
+
+//  Vuelos
+app.get('/vuelos', async (req, res) => {
+  try {
+    const result = await pool.query(`
+      SELECT v.codigo, v.ruta, v.estado, p.nombre AS piloto
+      FROM vuelos v
+      LEFT JOIN pilotos p ON v.piloto_id = p.id
+    `);
+
+    res.json(result.rows);
+
+  } catch (error) {
+    console.error('❌ ERROR EN VUELOS:', error);
+
+    res.status(500).json({
+      error: error.message
+    });
+  }
+});
+
+app.get('/pilotos', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT id, nombre FROM pilotos');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('❌ ERROR EN PILOTOS:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log(' Servidor en http://localhost:3000');
+});
+
+app.use(express.json());
+
+//  LOGIN SIMPLE
+app.post('/login', (req, res) => {
+  const { usuario, password } = req.body;
+
+  //  USUARIO HARDCODE
+  if (usuario === 'admin' && password === '1234') {
+    res.json({ ok: true });
+  } else {
+    res.status(401).json({ ok: false, mensaje: 'Credenciales incorrectas' });
+  }
+});
